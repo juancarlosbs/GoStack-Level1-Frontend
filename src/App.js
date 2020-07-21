705726b4b0679cbc40b6e618671e490de4c9e42d
juancarlosBS <juanrov_@hotmaail.com>
2020-07-21 16:19:06 -0300

    connect back-end and add new projects

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
+import api from './services/api'   
 
-import './App.css';
-import catIMG from './assets/cat-5394567_1920.jpg'
+import './App.css'
 
-import Header from './components/Header';
+import Header from './components/Header'
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web'])
+    const [projects, setProjects] = useState([])
 
-    function handleAddProject() {
-        setProjects([...projects, `Novo Projeto ${Date.now()}`])
+    useEffect(() => {
+        api.get('/projects').then(response => {
+            setProjects(response.data);
+        })
+    }, []) 
 
-        console.log(projects)
+    async function handleAddProject() {
+        //setProjects([...projects, `Novo Projeto ${Date.now()}`])
+        
+        const response = await api.post('/projects', {
+            title:"Frontend Project",
+            owner:"Juan Carlos"
+        })
+
+        const project = response.data
+
+        setProjects([...projects, project])
     }
 
     return (
         <>
             <Header title="Projects"/>
 
-            <img src={catIMG} />
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type='button' onClick={handleAddProject}>Adicionar projeto</button>
